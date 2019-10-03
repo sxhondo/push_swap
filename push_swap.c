e5d0cf0eb0237cf4fdd9f6fbc7b51a9255d1901d
sxhondo <sxhondo@student.42.fr>
2019-10-03 17:10:23 +0300

    quick sort

--- push_swap.c
+++ push_swap.c
@@ -12,59 +12,7 @@
 
 #include "push_swap.h"
 
-int				get_key_value(t_list **stack, unsigned medi)
-{
-
-	t_list		*tmp;
-
-	tmp = *stack;
-	while (medi--)
-		tmp = tmp->next;
-	return (*((int *)tmp->content));
-}
-
-int				find_lower(t_list **stack, int k_val)
-{
-	t_list		*tmp;
-	int 		i;
-	int 		j;
-
-	i = 1;
-	j = 0;
-	tmp = *stack;
-	while (tmp)
-	{
-		if ((*(int *)tmp->content < k_val))
-			return (i);
-		if ((*(int *)tmp->content) == k_val)
-			j = i;
-//		if (gl == 'g' && (*(int *)tmp->content > k_val))
-//			return (i);
-		tmp = tmp->next;
-		i++;
-	}
-	return (j ? j : 0);
-}
-
-/*	if (!(index = find_lower(a, k_val)))
-		return (0);
-	if (index <= llen / 2)
-		while (--index)
-		{
-			ft_printf("\nrot");
-			do_rot(a);
-			print_stacks(a, b);
-		}
-	else if (index > llen / 2)
-		while (index <= llen)
-		{
-			ft_printf("rev rot");
-			do_rev_rot(a);
-			print_stacks(a, b);
-			index++;
-		}*/
-
-
+/*
 int				brute_force(t_list **a, t_list **b)
 {
 	int 		top_a;
@@ -104,23 +52,91 @@ int				brute_force(t_list **a, t_list **b)
 	brute_force(a, b);
 	return (1);
 }
+*/
+
+int				get_key_value(t_list **stack, unsigned index)
+{
+
+	t_list		*tmp;
+
+	tmp = *stack;
+	while (index)
+	{
+		tmp = tmp->next;
+		index--;
+	}
+	return (*((int *)tmp->content));
+}
+
+int				find_lower(t_list **stack, int k_val)
+{
+	t_list		*tmp;
+	int 		i;
+	int 		j;
+
+	i = 1;
+	j = 0;
+	tmp = *stack;
+	while (tmp)
+	{
+		if ((*(int *)tmp->content < k_val))
+			return (i);
+		tmp = tmp->next;
+		i++;
+	}
+	return (0);
+}
+
+
+/* */
+int 		sort(t_list **a, t_list **b, int low, int high,
+													int index_m, int k_val)
+{
+	int 	i;
+	int 	t;
+
+	t = 0;
+	ft_printf("KEY_VALUE: %d\n", k_val);
+	print_stacks(a, b);
+	while (*((int*)(*a)->content) < k_val)
+		do_rot(a);
+	do_push(a, b, 'b');
+	quick_sort(a, b, low, high, index_m, k_val);
+}
+
+void			quick_sort(t_list **a, t_list **b,
+										int low, int high)
+{
+	int 		index_m;
+	int 		key_v;
+
+	/* pre_sort() must fill stack (b) with values lower than med-value from (a)
+	 * */
+
+//	pre_sort();
+	if (low < high)
+	{
+		index_m = (high - low) / 2;
+		key_v = get_key_value(a, index_m);
+		//sort_one();
+		quick_sort(a, b, low, index_m - 1); //sort (a)
+		quick_sort(a, b, index_m + 1, high); //sort (b)
+	}
+
+}
 
 void 			sort_stacks(int *nums, unsigned arg_am)
 {
 	t_list		*a;
 	t_list		*b;
 	int 		ops;
+	char 		st;
 
+	/* ops = brute_force(&a, &b); */
 	b = NULL;
 	a = fill_a(nums, arg_am);
+	sort_group(&a, &b, 0, ft_lstlen(&a));
 
-	ops = brute_force(&a, &b);
-
-	int i = ft_lstlen(&b);
-	ops += i;
-	while (i--)
-		do_push(&a, &b, 'a');
 	print_stacks(&a, &b);
-	ft_printf("Operations amount: %d\n", ops);
 	free_stacks(&a, &b);
 }
