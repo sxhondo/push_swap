57301d15d59b868c77dbbc2672c20986bbcee184
sxhondo <sxhondo@student.42.fr>
2019-10-11 21:33:14 +0300

    qs

--- push_swap.c
+++ push_swap.c
@@ -54,52 +54,6 @@ int				brute_force(t_list **a, t_list **b)
 }
 */
 
-/*
-int 			is_back_sorted(t_list **stack)
-{
-	t_list		*tmp;
-
-	tmp = *stack;
-	while (tmp->next)
-	{
-		if (*((int *)tmp->content) > *((int *)tmp->next->content))
-			tmp = tmp->next;
-		else
-			return (0);
-	}
-	return (1);
-}
-*/
-
-
-//void			merge_sort(t_list **a, t_list **b)
-//{
-//	int 		medi = ft_lstlen(a) / 2;
-//	int 		i = medi;
-//	print_stacks(a, b);
-//	while (medi--)
-//	{
-//		do_push(a, b, 'b');
-//		print_stacks(a, b);
-//	}
-//	while (i--)
-//	{
-//		int top_a = *((int *)(*a)->content);
-//		int top_b = *((int *)(*b)->content);
-//		if (top_a > top_b)
-//		{
-//			do_push(a, b, 'b');
-//			do_rot(b);
-//			do_push(a, b, 'a');
-//			do_rot(a);
-//			print_stacks(a, b);
-//		}
-//	}
-//	print_stacks(a, b);
-//
-//
-//}
-
 int 			is_sorted(t_list **stack)
 {
 	t_list		*tmp;
@@ -176,80 +130,117 @@ void			value_to_top(t_list **stack, int value)
 			do_rev_rot(stack);
 }
 
-void			partition(t_list **a, t_list **b, int k_val, char stack)
+
+//int				quick_sort(t_list **a, t_list **b,
+//										int left, int right, char stack)
+//{
+//	int 		llen = ft_lstlen(stack == 'a' ? a : b);
+//	int 		medi = llen / 2;
+//	int 		k_val;
+//
+//	if (stack == 'b' && is_back_sorted(b))
+//		return (0);
+//	if (stack == 'a' && is_sorted(a))
+//		return (0);
+//
+//	print_stacks(a, b);
+//
+//	k_val = get_value_for_index(stack == 'a' ? a : b, medi);
+//	value_to_top(stack == 'a' ? a : b, k_val);
+//	do_rot(stack == 'a' ? a : b);
+//
+//	print_stacks(a, b);
+//	partition(a, b, k_val, stack);
+//
+//	print_stacks(a, b);
+//	quick_sort(a, b, 0, ft_lstlen(b), 'b');
+//	quick_sort(a, b, ft_lstlen(a), 0, 'a');
+//}
+
+int 			check_max(t_list **stack, int k_val)
 {
+	t_list		*tmp;
+
+	tmp = *stack;
+	while (tmp)
+	{
+		if (*((int *)tmp->content) > k_val)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
+int				partition(t_list **a, t_list **b, int m_index)
+{
+	int 		k_val;
 	int 		top;
 	int 		i;
 
 	i = 0;
-	top = stack == 'a' ? *((int *)(*a)->content) : *((int *)(*b)->content);
-	while (top != k_val)
-	{
-		top = stack == 'a' ? *((int *)(*a)->content) : *((int *)(*b)->content);
-		if (top < k_val)
-		{
-			do_push(a, b, stack == 'a' ? 'b' : 'a');
-			i++;
-			continue;
-		}
-		if (top == k_val)
-		{
-			do_push(a, b, stack == 'a' ? 'b' : 'a');
-			i++;
-			break;
-		}
-		do_rot(stack == 'a' ? a : b);
-		print_stacks(a, b);
-	}
-//	if (stack == 'b')
-//		while (i--)
-//			do_push(a, b, 'b');
-	print_stacks(a, b);
-
-}
-
-void			pre_sort(t_list **a, t_list **b)
-{
-	int 		llen = ft_lstlen(a);
-	int 		medi = llen / 2;
-	int 		k_val;
+	top = *((int *)(*a)->content);
+	k_val = get_value_for_index(a, m_index);
 
 
-	k_val = get_value_for_index(a, medi);
 	value_to_top(a, k_val);
 	do_rot(a);
 
+	int llen = ft_lstlen(a);
 	print_stacks(a, b);
-	partition(a, b, k_val, 'a');
+
+	while (llen--)
+	{
+		top = *((int *)(*a)->content);
+		if (top > k_val)
+		{
+			do_push(a, b, 'b');
+			i++;
+			continue;
+		}
+		do_rot(a);
+		print_stacks(a, b);
+	}
+	do_rev_rot(a);
+	do_push(a, b, 'b');
+	print_stacks(a, b);
+	return (i + 1);
 }
 
-int				quick_sort(t_list **a, t_list **b,
-										int left, int right, char stack)
+void			quick_sort(t_list **a, t_list **b,
+								int n)
 {
-	int 		llen = ft_lstlen(stack == 'a' ? a : b);
-	int 		medi = llen / 2;
-	int 		k_val;
-
-	if (stack == 'b' && is_back_sorted(b))
-		return (0);
-	if (stack == 'a' && is_sorted(a))
-		return (0);
+	if (n == 1)
+		return;
+	int top_half_len = 0;
+	int tmp = 0;
+	int m_index = n / 2;
 
 	print_stacks(a, b);
+	top_half_len = partition(a, b, m_index);
+	print_stacks(a, b);
 
-	k_val = get_value_for_index(stack == 'a' ? a : b, medi);
-	value_to_top(stack == 'a' ? a : b, k_val);
-	do_rot(stack == 'a' ? a : b);
+	tmp = n - top_half_len;
+	while (tmp--)
+		do_rev_rot(a);
 
 	print_stacks(a, b);
-	partition(a, b, k_val, stack);
+	tmp = top_half_len;
+	while (tmp--)
+		do_push(a, b, 'a');
+	print_stacks(a, b);
+	quick_sort(a, b, top_half_len);
+
+
+	tmp = top_half_len;
+	while (tmp--)
+		do_rot(a);
 
 	print_stacks(a, b);
-	quick_sort(a, b, 0, ft_lstlen(b), 'b');
-	quick_sort(a, b, ft_lstlen(a), 0, 'a');
+	quick_sort(a, b, n - top_half_len);
+
+
+	exit (0);
 }
-
-
 
 void 			sort_stacks(int *nums, unsigned arg_am)
 {
@@ -262,11 +253,7 @@ void 			sort_stacks(int *nums, unsigned arg_am)
 	b = NULL;
 	a = fill_a(nums, arg_am);
 
-	pre_sort(&a, &b);
-	quick_sort(&a, &b, 0, ft_lstlen(&a), 'b');
-//	print_stacks(&a, &b);
+	quick_sort(&a, &b, ft_lstlen(&a));
 
-//	merge_sort(&a, &b);
-//	print_stacks(&a, &b);
 	free_stacks(&a, &b);
 }
